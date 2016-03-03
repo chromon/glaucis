173d3afdaa40f8998fef258fd8d5e2dba8111349
Ellery <allotory@msn.com>
2016-03-03 22:38:23 +0800

    Add: learn python day 2

---
+++ python/lxfPython/python09_tuple.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+s = ('a', 'b', [1, 2])
+print(s[0])
+print(s[2])
+s[2][0] = 3
+print(s)
+
+t = (1,)
+print(t[0])
---
+++ python/lxfPython/python10_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+s = [
+['apple', 'google', 'microsoft'],
+['java', 'python', 'ruby', 'php'],
+['adam', 'bart', 'lisa']
+]
+
+print(s[0][0])
+print(s[1][1])
+print(s[2][2])
