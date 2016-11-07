8e5edea949cfffcbf4a32603ba364c68c00d6d82
Ellery Zhang <allotory@msn.com>
2016-11-07 23:14:53 +0800

    Add: add 51node 1000

---
+++ 51Nod/src/basic/N1000.java
@@ -0,0 +1,36 @@
+package basic; /**
+ * 1000 A+B
+ * 给出2个整数A和B，计算两个数的和。
+ *
+ * Input
+ * 2个整数A B，中间用空格分割。（0 <= A, B <= 10^9）
+ *
+ * Output
+ * 输出A + B的计算结果。
+ *
+ * Input示例
+ * 1 2
+ *
+ * Output示例
+ * 3
+ *
+ * @author Ellery
+ * @Date 2016-11-06
+ */
+
+import java.util.Scanner;
+
+public class N1000 {
+
+    private static int add() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add());
+    }
+}
