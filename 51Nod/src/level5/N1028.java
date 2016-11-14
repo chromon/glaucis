712df70a66839fc38bd208ddf8623299d19aef3f
Ellery Zhang <allotory@msn.com>
2016-11-14 21:25:10 +0800

    Add: n1028

---
+++ 51Nod/src/level5/N1028.java
@@ -0,0 +1,42 @@
+package level5;
+
+/*
+    1028 大数乘法
+    基准时间限制：1 秒 空间限制：131072 KB
+    给出2个大整数A,B，计算A*B的结果。
+
+    Input
+    第1行：大数A
+    第2行：大数B
+    (A,B的长度 <= 100000，A,B >= 0）
+
+    Output
+    输出A * B
+
+    Input示例
+    123456
+    234567
+
+    Output示例
+    28958703552
+
+    Created by Ellery on 2016/11/11.
+ */
+
+/*
+    应该使用FFT（快速傅里叶算法）计算
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class N1028 {
+
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       BigInteger a = scanner.nextBigInteger();
+       BigInteger b = scanner.nextBigInteger();
+
+       System.out.println(a.multiply(b));
+    }
+}
