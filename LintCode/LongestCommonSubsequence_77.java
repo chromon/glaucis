4d970ae60d9e11c205178b844c2648f775e6fe17
Ellery Zhang <allotory@msn.com>
2019-02-02 20:00:25 +0800

    feat: add 77

---
+++ LintCode/LongestCommonSubsequence_77.java
@@ -0,0 +1,21 @@
+public class Solution {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: The length of longest common subsequence of A and B
+     */
+    public int longestCommonSubsequence(String A, String B) {
+        // write your code here
+        int n = A.length();
+        int m = B.length();
+        int f[][] = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                if(A.charAt(i - 1) == B.charAt(j - 1))
+                    f[i][j] = f[i - 1][j - 1] + 1;
+            }
+        }
+        return f[n][m];
+    }
+}
