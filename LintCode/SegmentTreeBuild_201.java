136199efa9f6d6d3e369f0340c95b5c8662341f2
Ellery Zhang <allotory@msn.com>
2019-02-06 19:15:42 +0800

    feat: add 201

---
+++ LintCode/SegmentTreeBuild_201.java
@@ -0,0 +1,37 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end) {
+ *         this.start = start, this.end = end;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param start: start value.
+     * @param end: end value.
+     * @return: The root of Segment Tree.
+     */
+    public SegmentTreeNode build(int start, int end) {
+        // write your code here
+        if(start > end) {  // check core case
+            return null;
+        }
+       
+        SegmentTreeNode root = new SegmentTreeNode(start, end);
+       
+        if(start != end) {
+            int mid = (start + end) / 2;
+            root.left = build(start, mid);
+            root.right = build(mid+1, end);
+           
+            // root.max = Math.max(root.left.max, root.right.max);
+        }
+        return root;
+    }
+}
