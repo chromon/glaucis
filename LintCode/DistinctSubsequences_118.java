09fbb0eb9a10ce182f17455be86ebfe13c687c8a
Ellery Zhang <allotory@msn.com>
2019-02-07 19:46:54 +0800

    feat: add 118

---
+++ LintCode/DistinctSubsequences_118.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     * @param S: A string
+     * @param T: A string
+     * @return: Count the number of distinct subsequences
+     */
+    public int numDistinct(String S, String T) {
+        // write your code here
+        if (S == null || T == null) {
+            return 0;
+        }
+
+        int[][] nums = new int[S.length() + 1][T.length() + 1];
+
+        for (int i = 0; i <= S.length(); i++) {
+            nums[i][0] = 1;
+        }
+        for (int i = 1; i <= S.length(); i++) {
+            for (int j = 1; j <= T.length(); j++) {
+                nums[i][j] = nums[i - 1][j];
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    nums[i][j] += nums[i - 1][j - 1];
+                }
+            }
+        }
+        return nums[S.length()][T.length()];
+    }
+}
