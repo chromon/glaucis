36370a9134026037f09123cf4954aaad18febe56
Ellery Zhang <allotory@msn.com>
2019-02-09 20:02:58 +0800

    feat: add 203

---
+++ LintCode/SegmentTreeModify_203.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, max;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int max) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.max = max
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of segment tree.
+     * @param index: index.
+     * @param value: value
+     * @return: nothing
+     */
+    public void modify(SegmentTreeNode root, int index, int value) {
+        // write your code here
+        if(root.start == index && root.end == index) { // 查找到
+            root.max = value;
+            return;
+        }
+        
+        // 查询
+        int mid = (root.start + root.end) / 2;
+        if(root.start <= index && index <=mid) {
+            modify(root.left, index, value);
+        }
+        
+        if(mid < index && index <= root.end) {
+            modify(root.right, index, value);
+        }
+        //更新
+        root.max = Math.max(root.left.max, root.right.max);
+    }
+}
