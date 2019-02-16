391829169a673350e4115b777df2525e18531358
Ellery Zhang <allotory@msn.com>
2019-02-16 19:00:38 +0800

    feat: add 148

---
+++ LintCode/SortColors_148.java
@@ -0,0 +1,33 @@
+public class Solution {
+    /**
+     * @param nums: A list of integer which is 0, 1 or 2 
+     * @return: nothing
+     */
+    public void sortColors(int[] a) {
+        if (a == null || a.length <= 1) {
+            return;
+        }
+        
+        int pl = 0;
+        int pr = a.length - 1;
+        int i = 0;
+        while (i <= pr) {
+            if (a[i] == 0) {
+                swap(a, pl, i);
+                pl++;
+                i++;
+            } else if(a[i] == 1) {
+                i++;
+            } else {
+                swap(a, pr, i);
+                pr--;
+            }
+        }
+    }
+    
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
