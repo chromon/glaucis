fb0f13def8ecf7a2c9722379103170f4eea57405
Ellery Zhang <allotory@msn.com>
2019-03-07 10:27:12 +0800

    feat: add no.219/217/232/225/226

---
+++ LeetCode/ContainsDuplicate_217.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
