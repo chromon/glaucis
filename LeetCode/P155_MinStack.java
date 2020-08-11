3ce411646e17087cdbac0a28eb755ae9e4dafd12
chromon <allotory@msn.com>
2020-08-11 14:38:57 +0800

    feat: add p155

---
+++ LeetCode/P155_MinStack.java
@@ -0,0 +1,84 @@
+/*
+155. Min Stack
+Easy
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+*/
+
+class MinStack {
+
+    Stack<Integer> data;
+    Stack<Integer> min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        data = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int x) {
+        data.push(x);
+        
+        if (min.isEmpty()) {
+            min.push(x);
+        } else {
+            if (min.peek() > x) {
+                min.push(x);
+            } else {
+                min.push(min.peek());
+            }
+        }
+    }
+    
+    public void pop() {
+        min.pop();
+        data.pop();
+    }
+    
+    public int top() {
+        return data.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
