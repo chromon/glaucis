2a538e2696504e87e68652178e943e3c05a9e836
chromon <allotory@msn.com>
2020-09-02 16:00:36 +0800

    feat: add p168/171

---
+++ LeetCode/P168_ExcelSheetColumnTitle.java
@@ -0,0 +1,44 @@
+/*
+168. Excel Sheet Column Title
+
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+Example 1:
+
+Input: 1
+Output: "A"
+Example 2:
+
+Input: 28
+Output: "AB"
+Example 3:
+
+Input: 701
+Output: "ZY"
+*/
+
+class Solution {
+    public String convertToTitle(int n) {
+        
+        // return n == 0 ? "" : convertToTitle(--n / 26) + (char)('A' + (n % 26));
+        
+        String res = "";
+        while (n != 0) {
+            char ch = (char)((n - 1) % 26 + 65);
+            n = (n - 1) / 26;
+            res = ch + res;
+        }
+        
+        return res;
+    }
+}
