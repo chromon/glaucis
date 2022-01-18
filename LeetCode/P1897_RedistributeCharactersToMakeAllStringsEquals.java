164824b6ebfce6a254e73d8b7890fc507dcebed2
Ellery Zhang <allotory@msn.com>
2022-01-18 10:22:01 +0800

    feat: add 1893/1897/1903/1909/1920/1913

---
+++ LeetCode/P1897_RedistributeCharactersToMakeAllStringsEquals.java
@@ -0,0 +1,54 @@
+/*
+1897. Redistribute Characters to Make All Strings Equal
+Easy
+
+You are given an array of strings words (0-indexed).
+
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","aabc","bc"]
+Output: true
+Explanation: Move the first 'a' in words[1] to the front of words[2],
+to make words[1] = "abc" and words[2] = "abc".
+All the strings are now equal to "abc", so return true.
+Example 2:
+
+Input: words = ["ab","a"]
+Output: false
+Explanation: It is impossible to make all the strings equal using the operation.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+*/
+
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int n = words.length;
+        int[] arr = new int[26];
+        
+        for (int i = 0; i < words.length; i++) {
+            char[] c = words[i].toCharArray();
+            for (int j = 0; j < c.length; j++) {
+                arr[c[j] - 'a']++;
+            }
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % n != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
