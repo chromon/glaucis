6252653558ea52aea06d476adad9e132ff268077
Ellery Zhang <allotory@msn.com>
2022-01-23 10:22:56 +0800

    feat: add 2068/2073/2078/2085/2089

---
+++ LeetCode/P2089_FindTargetIndicesAfterSortingArray.java
@@ -0,0 +1,78 @@
+/*
+2089. Find Target Indices After Sorting Array
+Easy
+
+You are given a 0-indexed integer array nums and a target element target.
+
+A target index is an index i such that nums[i] == target.
+
+Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,5,2,3], target = 2
+Output: [1,2]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The indices where nums[i] == 2 are 1 and 2.
+Example 2:
+
+Input: nums = [1,2,5,2,3], target = 3
+Output: [3]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 3 is 3.
+Example 3:
+
+Input: nums = [1,2,5,2,3], target = 5
+Output: [4]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 5 is 4.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i], target <= 100
+*/
+
+class Solution {
+    
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int start = 0;
+        int count = 0;
+        List<Integer> list = new ArrayList();
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (nums[i] == target) {
+                count++;
+            }
+            
+            if (nums[i] < target) {
+                start++;
+            }
+        }
+        
+        for (int i = start; i < start + count; i++) {
+            list.add(i);
+        }
+        
+        return list;
+    }
+    
+    /*
+    public List<Integer> targetIndices(int[] nums, int target) {
+        
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+    */
+}
