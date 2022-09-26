4b3b7aee2dd723d083f5b47a0d0994470a2b235e
Ellery Zhang <allotory@msn.com>
2022-09-26 16:10:12 +0800

    add 990

---
+++ LeetCode/P990_SatisfiabilityOfEqualityEquations.java
@@ -0,0 +1,74 @@
+/*
+990. Satisfiability of Equality Equations
+Medium
+
+You are given an array of strings equations that represent relationships between variables where each string equations[i] is of length 4 and takes one of two different forms: "xi==yi" or "xi!=yi".Here, xi and yi are lowercase letters (not necessarily different) that represent one-letter variable names.
+
+Return true if it is possible to assign integers to variable names so as to satisfy all the given equations, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: equations = ["a==b","b!=a"]
+Output: false
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+There is no way to assign the variables to satisfy both equations.
+Example 2:
+
+Input: equations = ["b==a","a==b"]
+Output: true
+Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+ 
+
+Constraints:
+
+1 <= equations.length <= 500
+equations[i].length == 4
+equations[i][0] is a lowercase letter.
+equations[i][1] is either '=' or '!'.
+equations[i][2] is '='.
+equations[i][3] is a lowercase letter.
+*/
+
+class Solution {
+    // 并查集
+    public boolean equationsPossible(String[] equations) {
+        int[] parent = new int[26];
+        
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
+        }
+        
+        for (String str : equations) {
+            if (str.charAt(1) == '=') {
+                int index1 = str.charAt(0) - 'a';
+                int index2 = str.charAt(3) - 'a';
+                union(parent, index1, index2);
+            }
+        }
+        
+        for (String str : equations) {
+            if (str.charAt(1) == '!') {
+                int index1 = str.charAt(0) - 'a';
+                int index2 = str.charAt(3) - 'a';
+                if (find(parent, index1) == find(parent, index2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        while (parent[index] != index) {
+            parent[index] = parent[parent[index]];
+            index = parent[index];
+        }
+        return index;
+    }
+}
